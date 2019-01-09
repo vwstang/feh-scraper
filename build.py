1a7ff578150cf02b9bbf260d658f2c2646e06af4
vwstang <vwstang6@gmail.com>
2019-01-08 19:20:20 -0500

    Updated to v0.0.2: New build function completed

--- build.py
+++ build.py
@@ -1,14 +1,19 @@
+# Build Module
+# 
+# This module contains all web scraping functions used by the manager in performing tasks to build and update the JSON database
+
+# Import module dependencies
 import requests
-from pprint import pprint
 from bs4 import BeautifulSoup
+
 
 def getHeroList():
   url = "https://feheroes.gamepedia.com/Hero_list"
   response = requests.get(url)
   rawHTML = BeautifulSoup(response.text, "html.parser")
   lstHeroHTML = rawHTML("tr",{"class": "hero-filter-element"})
-  print("Found " + str(len(lstHeroHTML)) + " heroes on Gamepedia.")
-  print("Now building hero database.")
+  print("Found " + str(len(lstHeroHTML)) + " heroes on Gamepedia")
+  print("Building hero database")
   dctHeroes = {}
   for heroTag in lstHeroHTML:
     dctHero = {}
@@ -21,3 +26,62 @@ def getHeroList():
       "gpedia": heroGPediaLink
     })
     dctHeroes[heroKey] = dctHero
+  print("Hero database build complete")
+  return dctHeroes
+
+
+def listStats(stringStats):
+  if len(stringStats) >= 5:
+    result = list(map(int,stringStats.split("/")))
+  else:
+    result = [int(stringStats)]
+  return result
+
+
+def getStats(statTable):
+  stats = {}
+  currentRarity = "0"
+  for i,tblData in enumerate(statTable):
+    if i % 7 == 0:
+      currentRarity = str(int(tblData.contents[0].encode("utf-8")))
+      stats.update({("rarity" + currentRarity): {}})
+    elif i % 7 == 1:
+      stats["rarity" + currentRarity].update({"HP": listStats(str(tblData.string))})
+    elif i % 7 == 2:
+      stats["rarity" + currentRarity].update({"ATK": listStats(str(tblData.string))})
+    elif i % 7 == 3:
+      stats["rarity" + currentRarity].update({"SPD": listStats(str(tblData.string))})
+    elif i % 7 == 4:
+      stats["rarity" + currentRarity].update({"DEF": listStats(str(tblData.string))})
+    elif i % 7 == 5:
+      stats["rarity" + currentRarity].update({"RES": listStats(str(tblData.string))})
+    elif i % 7 == 6:
+      pass
+    else:
+      print("OVERFLOW ERROR: Table has more than 7 columns.")
+  return stats
+
+
+def getSkills(skillTables):
+  skills = []
+  for skillTypes in skillTables:
+    for i, skillRow in enumerate(skillTypes("tr")):
+      if i > 0:
+        skills.append("".join(skillRow.find("a").string.lower().split(" ")))
+  return skills
+
+
+def getHeroData(url):
+  response = requests.get(url)
+  print(".",end="",flush=True)
+  soup = BeautifulSoup(response.text, "html.parser")
+  print(".",end="",flush=True)
+  heroData = {}
+  lvl1Stats = getStats(soup.find("span",{"id": "Level_1_stats"}).parent.next_sibling.find_all("td"))
+  lvl40Stats = getStats(soup.find("span",{"id": "Level_40_stats"}).parent.next_sibling.find_all("td"))
+  defaultSkills = getSkills(soup("table",{"class": "skills-table"}))
+  print(".",end="",flush=True)
+  heroData.update({"lvl1stats": lvl1Stats,
+                   "lvl40stats": lvl40Stats,
+                   "defaultskills": defaultSkills})
+  return heroData
--- feheromanager.py
+++ feheromanager.py
@@ -1,4 +1,13 @@
-# Import libraries
+# Fire Emblem Heroes - Hero Database Manager
+# Main App Module
+# 
+# v0.0.2
+# 
+# Controls tasks to run based on user input
+
+# Import module dependencies
+from pprint import pprint
+import time
 import json
 import build
 
@@ -16,27 +25,77 @@ def comparing():
   dbComparer(currentHeroList,updatedHeroList)
 
 
-def writeToJSON(dictionary, name):
+def writeToJSON(dictionary,name):
   filename = str(name) + ".json"
   with open(filename,"w") as outfile:
     json.dump(dictionary,outfile)
-  print("Output to " + filename + " completed.")
+  print("Output to " + filename + " complete")
+
+
+def runBuild():
+  heroList = build.getHeroList()
+  print("Delay between server requests set to 1 second")
+  estOpTime = int(len(sorted(heroList))/60)
+  if estOpTime == 1:
+    print("Please wait - this operation may take over a minute")
+  elif estOpTime > 1:
+    print("Please wait - this operation may take over %s minutes"%(estOpTime))
+  for hero in heroList:
+    time.sleep(1)
+    print("Fetching data for " + hero,end="",flush=True)
+    heroList[hero].update(build.getHeroData(heroList[hero]["gpedia"]))
+    print("Done")
+  writeToJSON(heroList,"herodb")
+  print("Task complete")
+
+
+def runQuickUpd():
+  # with open("herodb.json") as file:
+  #   currentHeroList = json.load(file)
+  print("I'm in runQuickUpd!")
+
+
+def runFullUpd():
+  print("I'm in runFullUpd!")
+
+
+def runExit():
+  print("Exiting manager")
+
+
+def runDebug():
+  print("I'm in the (hidden) runDebug!")
 
 
 def main():
   welcomemsg = """
-******************************************
-FIRE EMBLEM HEROES - HERO DATABASE MANAGER
-******************************************
+******************************************************
+***** FIRE EMBLEM HEROES - HERO DATABASE MANAGER *****
+******************************************************
 
-Available commands:
+v0.0.2
 
-build     build new database
+=== Available tasks ===
 
+build       build new database
+q-update    quickly update database for new heroes
+f-update    fully check database for new heroes and changes to existing heroes
+exit        don't run any tasks and exit
 
+===
 """
+  task = {
+    "build": runBuild,
+    "q-update": runQuickUpd,
+    "f-update": runFullUpd,
+    "exit": runExit,
+    "debug": runDebug
+  }
   print(welcomemsg)
-  task = input("Your command? ").lower()
-  print(task)
+  userTask = input("What task would you like to run? ").lower()
+  try:
+    task[userTask]()
+  except KeyError:
+    print("INVALID COMMAND: Please rerun the script and enter a valid command")
 
 main()
